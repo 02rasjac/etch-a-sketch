25860a6b8b14f5871b0c778cbb53e888fe853d6b
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-01-28 13:56:56 +0100

    Add button to change grid size

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 // Generate a grid of divs
 let etch = document.querySelector('#etch');
-for (let i = 0; i < 16 * 16; i++) {
-  let div = document.createElement('div');
-  div.classList.add('pixel');
-  div.addEventListener('mouseover', color);
-  etch.appendChild(div);
+document.querySelector('button').addEventListener('click', () => {
+  let input = prompt('Change the grid size (default is 16). Enter only a number, the grid is square.');
+  if (!isNaN(input)) {
+    console.log('test');
+    generateGrid(input);
+  }
+});
+
+function generateGrid(gridSize = 16) {
+  etch.textContent = '';
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let div = document.createElement('div');
+    div.classList.add('pixel');
+    div.addEventListener('mouseover', color);
+    etch.appendChild(div);
+  }
 }
 
 function color(e) {
   e.target.classList.add('colored');
 }
+
+generateGrid();
