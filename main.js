19013e18061e35b071269c1f6c442cb1a98d2865
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-01-28 13:41:36 +0100

    Add permanent coloring of divs
    Instead of changing color when hovering over div/pixel through CSS, it
    adds a new class permanently to the div through JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+// Generate a grid of divs
 let etch = document.querySelector('#etch');
 for (let i = 0; i < 16 * 16; i++) {
   let div = document.createElement('div');
   div.classList.add('pixel');
+  div.addEventListener('mouseover', color);
   etch.appendChild(div);
+}
+
+function color(e) {
+  e.target.classList.add('colored');
 }
