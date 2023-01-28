99b89cc967cc7a883f33c5d4f92347309342c27a
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-01-28 14:11:02 +0100

    Fix grid resizing
    - Made the CSS-grid a fixed size
    - Changes the ammount of rows/columns in the grid through javascript
        to not get gaps between divs
    - Compute the width of the divs based on the grid size and the
        grid's width

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Generate a grid of divs
 let etch = document.querySelector('#etch');
 document.querySelector('button').addEventListener('click', () => {
   let input = prompt('Change the grid size (default is 16). Enter only a number, the grid is square.');
@@ -10,9 +9,14 @@ document.querySelector('button').addEventListener('click', () => {
 
 function generateGrid(gridSize = 16) {
   etch.textContent = '';
+  etch.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  etch.style.gridTemplateRow = `repeat(${gridSize}, 1fr)`;
+
+  const divSize = 960 / gridSize;
   for (let i = 0; i < gridSize * gridSize; i++) {
     let div = document.createElement('div');
-    div.classList.add('pixel');
+    div.style.width = `${divSize}px`;
+    div.style.height = `${divSize}px`;
     div.addEventListener('mouseover', color);
     etch.appendChild(div);
   }
